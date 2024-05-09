10c60394fefcd647bc5e5752120e65db91e3b113
eucarizan <eucarizan@gmail.com>
2024-05-09 20:55:26 +0800

    refactor: min and max instant units

--- date_and_time/GetMaxMinusMin/src/main/java/dev/nj/solutions/GetMaxMinusMin.java
+++ date_and_time/GetMaxMinusMin/src/main/java/dev/nj/solutions/GetMaxMinusMin.java
@@ -1,13 +1,15 @@
 package dev.nj.solutions;
 
 import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Collections;
 import java.util.List;
 
 public class GetMaxMinusMin {
     public static long getMaxMinusMin(List<Instant> instantList) {
-        //return instantList.stream().max(Instant::compareTo).get().minusSeconds(instantList.stream().min(Instant::compareTo).get());
-        Instant time1 = instantList.stream().max(Instant::compareTo).orElse(instantList.get(0));
-        Instant time2 = instantList.stream().min(Instant::compareTo).orElse(instantList.get(0));
-        return time1.getEpochSecond() - time2.getEpochSecond();
+        // Instant time1 = instantList.stream().max(Instant::compareTo).orElse(instantList.get(0));
+        // Instant time2 = instantList.stream().min(Instant::compareTo).orElse(instantList.get(0));
+        // return time1.getEpochSecond() - time2.getEpochSecond();
+        return Collections.min(instantList).until(Collections.max(instantList), ChronoUnit.SECONDS);
     }
 }
