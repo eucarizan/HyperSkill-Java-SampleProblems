095c9e2eaa07b142df17ac7560e0635186efb1b2
eucarizan <eucarizan@gmail.com>
2024-05-15 15:27:47 +0800

    string reverse, lambda

--- oop/StringReverser/src/main/java/dev/nj/solutions/ReverseString.java
+++ oop/StringReverser/src/main/java/dev/nj/solutions/ReverseString.java
@@ -2,16 +2,7 @@ package dev.nj.solutions;
 
 public class ReverseString {
     public static String reverse(String str) {
-        StringReverser reverser = new StringReverser() {
-            @Override
-            public String reverse(String str) {
-                StringBuilder out = new StringBuilder();
-                for (int i = str.length() - 1; i >= 0; i--) {
-                    out.append(str.charAt(i));
-                }
-                return out.toString();
-            }
-        };
+        StringReverser reverser = str1 -> new StringBuilder(str1).reverse().toString();
         return reverser.reverse(str);
     }
 }
